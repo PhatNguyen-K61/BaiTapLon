7b0c7c050724e434d414cce662a00653249b72d0
trantrongnhan466 <79680125+trantrongnhan466@users.noreply.github.com>
2021-04-21 15:01:50 +0700

    Nhân: Thêm hàm menu

--- quanLyThuVien.cpp
+++ quanLyThuVien.cpp
@@ -48,6 +48,7 @@ int main()
     int id_need_to_find;
     bookList = (book_st *)malloc(sizeof(book_st));
     system("color b0");
+    menu(file, output_path, bookList, numberBooks, totalBooks, search, id_need_to_find);
     if(bookList != NULL){
         free(bookList);
     }
